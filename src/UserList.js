65b41de61f43d09770b5483f7e96c06af59fc799
SergeyAndrienko <64705199+SergeyAndrienko@users.noreply.github.com>
2021-10-18 22:20:37 +0300

    Add files via upload

---
+++ src/UserList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import User from './User.js'
+import UserInfo from './UserInfo.js'
+
+
+export default class UserList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: props.users,
+            isUserInfoActive: false,
+            currentUser: {}
+        }
+    }
+
+    handleClick = (user) => {
+        this.setState({currentUser: user, isUserInfoActive: true})
+    }
+
+    // renderUser = () => {
+    //
+    //     return JSON.stringify(this.state.currentUser)
+    // };
+
+    render() {
+        return (
+            <div className='wrap'>
+                <ul>
+                    {this.state.users.map(user => <User key={user.name} user={user} handleOnClick={this.handleClick}/>)}
+                </ul>
+                {/*<div className={this.state.isUserInfoActive ? "visible" : "invisible"}>*/}
+
+                {/*    {this.renderUser()}*/}
+                {/*</div>*/}
+                {this.state.isUserInfoActive ? <UserInfo user={this.state.currentUser}/> : <></>}
+            </div>
+        )
+    }
+}
